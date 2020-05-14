cdc37ea7c62d3cc8c0265a1e85128b3b2e23b40f
2S Andre Luiz <andreluizalc@fab.mil.br>
2020-05-14 14:52:11 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { LogService } from './shared/log.service';
+import { CursosService } from './cursos/cursos.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CriarCursoModule} from './criar-curso/criar-curso.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [],
+
+  providers: [LogService],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cursos/cursos.service.ts
@@ -0,0 +1,30 @@
+import { LogService } from './../shared/log.service';
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable()
+
+export class CursosService {
+
+    emitirCursoCriado = new EventEmitter<string>();
+    static criouNovoCurso = new EventEmitter<string>();
+
+    private cursos: string[] = ['Angular 2', 'Java', 'Phonegap'];
+
+    constructor(private logService: LogService){
+        console.log('CursoServiço')
+    }
+
+    getCursos() {
+        this.logService.consoleLog('Obtendo lista de curso')
+        return  this.cursos;
+        
+    }
+
+    addCurso(curso: string) {
+        this.logService.consoleLog(`Criando um novo curso ${curso}`)
+        this.cursos.push(curso);
+        this.emitirCursoCriado.emit(curso);
+        CursosService.criouNovoCurso.emit(curso);
+    }
+
+}
